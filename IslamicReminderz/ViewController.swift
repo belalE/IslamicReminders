48e9a5dff38f7020a381dabd29acdd730502fd2a
Belal E <balolyman1234@gmail.com>
2018-11-05 11:50:46 -0600

    Initial Commit

---
+++ IslamicReminderz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IslamicReminderz
+//
+//  Created by Belal Elsiesy on 11/5/18.
+//  Copyright © 2018 Elsiesy Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
